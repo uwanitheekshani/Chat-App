f5025740235db484ccbc98aad24cbea33e2b7a0e
uwanitheekshani <uwanitheekshani@gmail.com>
2022-08-08 22:32:10 +0530

    Create Client01,02,03

--- src/Client_02.java
+++ src/Client_02.java
@@ -7,7 +7,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 
-public class AppInitializer extends Application {
+public class Client_02 extends Application {
 
     public static void main(String[] args) {
         launch(args);
--- src/UserLoginFormController.java
+++
@@ -1,6 +0,0 @@
-import javafx.event.ActionEvent;
-
-public class UserLoginFormController {
-    public void btnLogInOnAction(ActionEvent actionEvent) {
-    }
-}
