3ff22e2bea640f1c456a2bd1801586551c0a90f7
uwanitheekshani <uwanitheekshani@gmail.com>
2022-08-08 23:02:38 +0530

    Create Server class

--- src/controller/ChatRoomFormController.java
+++ src/controller/ChatRoomFormController.java
@@ -1,5 +1,18 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
 public class ChatRoomFormController {
 
+    public AnchorPane chatRoomContext;
+    public JFXTextArea txTextArea;
+    public JFXTextField txtMessage;
+    public Label lblName;
+
+    public void btnSendOnAction(ActionEvent actionEvent) {
+    }
 }
